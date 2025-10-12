67fd8ee03565d08708b32376d3689b2e38e2dbe5
Carlos <herrera4345@gmail.com>
2025-10-11 21:07:55 -0400

    First Commit

---
+++ perlite/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'secure-77/perlite',
+        'pretty_version' => '1.5.9',
+        'version' => '1.5.9.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'erusev/parsedown' => array(
+            'pretty_version' => '1.7.4',
+            'version' => '1.7.4.0',
+            'reference' => 'cb17b6477dfff935958ba01325f2e8a2bfa6dab3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../erusev/parsedown',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mindplay/php-vite' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => 'f9416949acd4652d322793055e2e7661648bd69c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mindplay/php-vite',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'secure-77/perlite' => array(
+            'pretty_version' => '1.5.9',
+            'version' => '1.5.9.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'carlos/perltie-vite-demo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7961eda4099d8f179224cc7c99de9b31c2a537bf',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'carlos/perltie-vite-demo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7961eda4099d8f179224cc7c99de9b31c2a537bf',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
